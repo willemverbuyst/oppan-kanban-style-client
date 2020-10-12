75a3ff45fd3f6aaf30465031cd4c1b15403120e1
Willem Verbuyst <willemverbuyst@gmail.com>
2020-10-12 06:27:00 +0200

    Move dummy data to separate file

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,9 @@
   import { crossfade } from 'svelte/transition';
   import { flip } from 'svelte/animate';
   import Header from './Header.svelte';
+  import { dummyData } from './dummy-data';
 
+  const tasks = [...dummyData];
   const [send, receive] = crossfade({
     fallback(node, params) {
       const style = getComputedStyle(node);
@@ -19,22 +21,6 @@
       };
     },
   });
-
-  let tasks = [
-    { id: 1, status: 'done', description: 'Consult the docs' },
-    { id: 2, status: 'done', description: 'Learn SQL' },
-    {
-      id: 3,
-      status: 'in-progress',
-      done: true,
-      description: 'Study TypeScript',
-    },
-    { id: 4, status: 'todo', description: 'Daily stand-up' },
-    { id: 5, status: 'todo', description: 'Read MDN' },
-    { id: 6, status: 'review', description: 'Fix bugs' },
-    { id: 7, status: 'todo', description: 'Learn Sass' },
-    { id: 8, status: 'in-progress', description: 'Make Svelte app' },
-  ];
 
   let uid = tasks.length + 1;
 
