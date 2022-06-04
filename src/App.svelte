67d366635122c61be2ef468110b9b8faf565f47e
Willem Verbuyst <willemverbuyst@gmail.com>
2022-06-04 05:10:47 +0200

    Create TaskInProgress component

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,9 @@
   import { crossfade } from 'svelte/transition';
   import { flip } from 'svelte/animate';
   import Header from './Header.svelte';
-  import NewTodo from './NewTodo.svelte'
+  import NewTodoInput from './NewTodoInput.svelte'
+  import TaskInProgress from './TaskInProgress.svelte';
+  import TaskTodo from './TaskTodo.svelte';
   import { dummyData } from './dummy-data';
 
   let tasks = [...dummyData];
@@ -26,7 +28,7 @@
 
   let uid = tasks.length + 1;
 
-  const add = (event) => {
+  function add(event) {
     const task = {
       id: uid++,
       status: 'todo',
@@ -36,8 +38,8 @@
     tasks = [task, ...tasks];
   };
 
-  const remove = (task) => {
-    tasks = tasks.filter((t) => t !== task);
+  function remove(task) {
+    tasks = tasks.filter((t) => t.id !== task.detail.taskId);
   };
 </script>
 
@@ -78,8 +80,7 @@
     opacity: 1;
   }
 
-  .todo,
-  .in-progress,
+  
   .review,
   .done {
     float: left;
@@ -93,12 +94,7 @@
     margin: 0 auto;
   }
 
-  .todo label {
-    background-color: #f2f2f0;
-  }
-  .in-progress label {
-    background-color: #ffbc47;
-  }
+  
   .review label {
     background-color: #f65e5d;
     color: #fff;
@@ -110,37 +106,13 @@
 </style>
 
 <Header />
-<NewTodo on:addTodo={add}/>
+<NewTodoInput on:addTodo={add}/>
 <div class="board">
-  <div class="todo">
-    <h2>todo</h2>
-    {#each tasks.filter((t) => t.status === 'todo') as task (task.id)}
-      <label
-        in:receive={{ key: task.id }}
-        out:send={{ key: task.id }}
-        animate:flip>
-        <input
-          type="checkbox"
-          on:change={() => (task.status = 'in-progress')} />
-        {task.description}
-        <button on:click={() => remove(task)}>x</button>
-      </label>
-    {/each}
-  </div>
+  <TaskTodo bind:tasks={tasks} on:removeTask={remove}/>
+  <TaskInProgress bind:tasks={tasks} on:removeTask={remove}/>
 
-  <div class="in-progress">
-    <h2>in progress</h2>
-    {#each tasks.filter((t) => t.status === 'in-progress') as task (task.id)}
-      <label
-        in:receive={{ key: task.id }}
-        out:send={{ key: task.id }}
-        animate:flip>
-        <input type="checkbox" on:change={() => (task.status = 'review')} />
-        {task.description}
-        <button on:click={() => remove(task)}>x</button>
-      </label>
-    {/each}
-  </div>
+
+  
 
   <div class="review">
     <h2>review</h2>
