5a795c6761aad570c067050cc2b78a8f2ac77dd2
Willem Verbuyst <willemverbuyst@gmail.com>
2022-08-25 20:59:04 +0200

    Add toggle

--- client/src/ui/Header.svelte
+++ client/src/ui/Header.svelte
@@ -1,14 +1,29 @@
-<div>
-  <h1>Oppan Kanban Style</h1>
+<script>
+  import Toggle from './Toggle.svelte';
+
+  let acceptTerms = false;
+</script>
+
+<div class="header">
+  <div class="header__element">
+    <h1>Oppan Kanban Style</h1>
+  </div>
+  <div class="header__element">
+    <Toggle bind:checked={acceptTerms} />
+  </div>
 </div>
 
 <style>
-  div {
+  .header {
     color: #fff;
     display: flex;
-    padding: 0.7rem 2rem;
+    justify-content: space-between;
+    align-items: center;
     width: 100vw;
-    margin-bottom: 1rem;
     background-color: #1d2d60;
+  }
+
+  .header__element {
+    padding: 0.5rem 2rem;
   }
 </style>
